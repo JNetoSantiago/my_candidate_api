a934ee29a6f7fc6520e6594f441077911b699f19
João Neto <joaodeveloper29@gmail.com>
2025-06-01 21:16:17 -0300

    feature: improve csv upload controller and test

--- app/controllers/api/v1/csv_upload_controller.rb
+++ app/controllers/api/v1/csv_upload_controller.rb
@@ -1,14 +1,20 @@
 module Api
   module V1
     class CsvUploadController < ApplicationController
+      STATE = "PI".freeze
+
       def handler
         if params[:file].present?
+          upload_id = SecureRandom.uuid
+
+          upload = CsvUpload.create!(upload_id:, state: STATE, status: :queued)
+
           tmp_path = Rails.root.join("tmp", "csv_#{SecureRandom.uuid}.csv")
           File.open(tmp_path, "wb") { |f| f.write(params[:file].read) }
 
-          CsvUploadJob.perform_later(tmp_path.to_s)
+          CsvUploadJob.perform_later(tmp_path.to_s, upload.id)
 
-          render json: { message: "Arquivo CSV está sendo processado!" }, status: :ok
+          render json: { message: "Arquivo CSV está sendo processado!", upload_id: upload_id }, status: :ok
         else
           render json: { error: "É necessário informar um arquivo CSV!" }, status: :unprocessable_entity
         end
